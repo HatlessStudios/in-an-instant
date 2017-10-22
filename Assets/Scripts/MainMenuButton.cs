c8cb8031fe3421840853a4bd98f5f826069c47be
Scott Richmond-Wood <Scott@Scotts-MacBook-Air.local>
2017-10-22 11:57:45 +0100

    Added pause and menu buttons

---
+++ Assets/Scripts/MainMenuButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButton : MonoBehaviour {
+
+	public Button button;
+	public AudioSource audio;
+
+	void Start()
+	{
+		Button btn = GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+		StartCoroutine(playSoundThenLoad());
+	}
+
+	IEnumerator playSoundThenLoad()
+	{
+		audio.Play();
+		yield return new WaitForSeconds(audio.clip.length - 0.3f);
+		SceneManager.LoadScene("Menu");
+	}
+}
