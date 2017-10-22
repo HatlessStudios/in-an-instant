cab71c581f89db5a39a27a585e611a1501d4c998
Alexandre Tunstall <alexandrejamestunstall@gmail.com>
2017-10-22 06:38:01 +0100

    Improve physics

--- Assets/Scripts/EnergyNode.cs
+++ Assets/Scripts/EnergyNode.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using UnityEngine;
 
 public class EnergyNode : MonoBehaviour {
-    public const double STRENGTH = 10;
+    public const double STRENGTH = 20;
 
     public float gravity
     {
@@ -49,6 +49,7 @@ public class EnergyNode : MonoBehaviour {
     private NodeBuilder parent;
     private Rigidbody body;
     private Vector3 trueVelocity;
+    private Rigidbody bodySim;
 
     private void Start()
     {
@@ -56,13 +57,19 @@ public class EnergyNode : MonoBehaviour {
         body = GetComponent<Rigidbody>();
         halo = (Behaviour) GetComponent("Halo");
         circle = GetComponent<DrawCircle>();
+        trueVelocity = body.velocity;
+        bodySim = Instantiate(Resources.Load<GameObject>("Prefabs/PhysicsSim"), Vector3.zero, Quaternion.identity).GetComponent<Rigidbody>();
+        bodySim.transform.parent = transform;
     }
 
     void Update()
     {
-        if (parent.paused) return;
+        if (parent.paused || lockPosition) return;
         EnergyNode[] nodes = transform.parent.GetComponentsInChildren<EnergyNode>();
-        double timeFlow = GetTimeFlow(nodes) * Time.deltaTime;
+        double rawTimeFlow = GetTimeFlow(nodes);
+        double timeFlow = rawTimeFlow * Time.deltaTime;
+        body.AddForce(-body.velocity, ForceMode.VelocityChange);
+        bodySim.AddForce(-bodySim.velocity, ForceMode.VelocityChange);
         foreach (EnergyNode node in nodes.Where(n => n.transform.position != transform.position))
         {
             double nodeTimeFlow = timeFlow * node.GetTimeFlow(nodes);
@@ -70,13 +77,14 @@ public class EnergyNode : MonoBehaviour {
             Vector3 path = node.transform.position - transform.position;
             if (path.magnitude > node._radius) continue;
             path /= path.sqrMagnitude;
-            body.AddForce((float) (STRENGTH * _gravity * node._gravity) * path, ForceMode.Impulse);
-            body.AddForce((float) (-STRENGTH * _charge * node._charge) * path, ForceMode.Impulse);
+            bodySim.AddForce((float) (STRENGTH * _gravity * node._gravity) * path, ForceMode.Impulse);
+            bodySim.AddForce((float) (-STRENGTH * _charge * node._charge) * path, ForceMode.Impulse);
         }
-        trueVelocity += body.velocity;
-        body.velocity = Vector3.zero;
-        transform.position += (float) timeFlow * trueVelocity;
-	}
+        trueVelocity += bodySim.velocity;
+        body.AddForce((float) rawTimeFlow * trueVelocity, ForceMode.VelocityChange);
+        //body.AddForce(-trueVelocity, ForceMode.VelocityChange);
+        //if (((float) rawTimeFlow * trueVelocity).magnitude != 0F) Debug.Log("Time Flow: " + rawTimeFlow + " | Velocity: " + body.velocity);
+    }
 
     void OnMouseDown()
     {
