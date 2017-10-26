bd57bc51ae1565383075fcc32cf28a4f0b17b2e3
Scott Richmond-Wood <Scott@Scotts-MacBook-Air.local>
2017-10-26 15:36:26 +0100

    Fixes #3

---
+++ Assets/Scripts/HideUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HideUI : MonoBehaviour {
+
+	public GameObject objectToBeHidden;
+
+	// Use this for initialization
+	void Start () {
+		Destroy(objectToBeHidden);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
